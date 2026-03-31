718a5175a3196ff71f7762be7a562f2616115788
mmorell.dev <mauricio_morell@unicomer.com>
2026-03-31 19:32:37 -0300

    fix3

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from "stripe";
 const secretKey = process.env.STRIPE_SECRET_KEY;
 
 export const stripe = secretKey
-  ? new Stripe(secretKey, { apiVersion: "2023-10-16" })
+  ? new Stripe(secretKey, { apiVersion: "2026-03-25.dahlia" })
   : null;
